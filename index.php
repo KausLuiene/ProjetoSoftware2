4c59650ea9a66adef9b1279ab0294b0195e36360
KausLuiene <54158165+KausLuiene@users.noreply.github.com>
2019-10-16 17:03:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -40,16 +40,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script>
-    function notificarFrase() {
-        new Notification ('AlfabetizaJunto tem uma informação para você')
-      }
-
-    notificarFrase();
-    if (Notification.permission !== "granted") {
-         Notification.requestPermission();
-    } 
-    </script>
-<script>
     let divFrases = document.getElementById('frases'); //coloca o elemento com id frases na variavel divFrases
     let options = {method: 'GET'}; //opções pra função fetch, nesse caso só tem o método que é GET
     fetch('https://spreadsheets.google.com/feeds/list/1wlq5rZEfXLtSDsAEvPAur9Oos0zBK3MVm3fODGppQUA/od6/public/values?alt=json', options) // usa a função fetch para ler os dados da url do json da planilha
@@ -77,7 +67,15 @@
 
             });
         });
+<script>
+    function notificarFrase() {
+        new Notification ('AlfabetizaJunto tem uma nova informação para você')
+      }
 
+    if (Notification.permission !== "granted") {
+         Notification.requestPermission().then(() => notificarFrase(););
+    }  else { notificarFrase();}
+    </script>
 
     </script>
     
